b8d56fe85d51a6a3082ca38090ad0b4fc0544a29
Natalieliu406 <100347589+Natalieliu406@users.noreply.github.com>
2022-02-24 18:38:35 +0800

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    printf("I am %d year old \n;",15);
+    printf("my weight %d kg \n;",50);
+    printf("my height is %d cm \n;",165);
+    printf("I have %d sisters %d brothers \n;",1,0);
+    
+    cout << "I am "<< 15 <<"yers old";
+    cout << "my weight is "<< 50 <<"kg";
+    cout << "my height is "<< 165 <<"cm";
+    cout << "I have "<< 1 <<"sisters"<< 0 <<"brothers";
+   
+   
+    return 0;
+}
